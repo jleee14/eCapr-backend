805ea1dbd53cd1f9198a498955555e1595ba738b
jleee14 <kjleee14@gmail.com>
2022-04-09 14:42:22 -0700

    add users app, secretkeys and settings configurations

--- eCapr_back/wsgi.py
+++ eCapr_back/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for eCapr_back project.
+WSGI config for ecapr_back project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eCapr_back.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecapr_back.settings')
 
 application = get_wsgi_application()
