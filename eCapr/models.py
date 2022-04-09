df28a4579839d08bc1bd08b48ddec0336e08b67c
jleee14 <kjleee14@gmail.com>
2022-04-09 14:01:19 -0700

    create bet model, django app

---
+++ eCapr/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Bet(models.Model):
+    bookmaker = models.CharField(max_length=50)
+    name = models.CharField(max_length=200)
+    odds = models.IntegerField()
+    bet_type = models.CharField(max_length=50)
+    bet_market = models.CharField(max_length=75)
+    wager = models.IntegerField()
+    sport = models.CharField(max_length=50)
+    league = models.CharField(max_length=50)
+    # owner = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    notes = models.TextField()
+    date_placed = models.DateTimeField(auto_now_add=True, auto_now=False)
+    date_resolved = models.DateTimeField(auto_now_add=False, auto_now=True)
+    resolved = models.BooleanField()
+    bet_result = models.CharField(max_length=10, default='None')
+
+
+    def __str__(self):
+        return self.name
---
+++ settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE ecapr;
+CREATE USER ecapruser WITH PASSWORD 'ecapr';
+GRANT ALL PRIVILEGES ON DATABASE ecapr to ecapruser;
